f050df8382f999b9df73b781d50c31593ba0a81c
Amey Gurjar <techanimationstudio@gmail.com>
2022-09-10 23:54:56 +0530

    AmeyChat

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QVBoxLayout
 from sys import argv as sys_argv, exit as sys_exit
 from screeninfo import get_monitors
 from socket import socket
 
 class AmeyChat:
     def __init__(self):
-        self.WindowResoltion = (get_monitors()[0].width, get_monitors()[0].height)
+        self.WindowResoltion = (int(get_monitors()[0].width/2), int(get_monitors()[0].height/2))
         self.AmeyChatApp = QApplication(sys_argv)
         self.AmeyGUIWindow = QMainWindow()
         self.AmeyGUIWindow.setWindowTitle("AmeyChat")
-        self.AmeyGUIWindow.setGeometry(int(self.WindowResoltion[0]/4.2666), int(self.WindowResoltion[1]/4.32), int(self.WindowResoltion[0]/2), int(self.WindowResoltion[1]/2))
+        self.AmeyGUIWindow.setGeometry(int(get_monitors()[0].width/4.2666), int(get_monitors()[0].height/4.32), self.WindowResoltion[0], self.WindowResoltion[1])
         
+    def mainChatInterface(self):
+        mainWidget = QVBoxLayout()
+        self.ChatTextBox = QLineEdit(self.AmeyGUIWindow)
+        self.ChatTextBox.move(100, 100)
+        self.ChatTextBox.resize(100, 100)
+        mainWidget.addWidget(self.ChatTextBox)
+        self.AmeyGUIWindow.setLayout(mainWidget)
 if __name__ == "__main__":
     A = AmeyChat()
     A.AmeyGUIWindow.show()
-    print(A.WindowResoltion)
     sys_exit(A.AmeyChatApp.exec_())
